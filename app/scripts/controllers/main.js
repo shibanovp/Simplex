85d932d60692f2ff69d80e1732a37ccc0c919735
Paul <unkindest89@gmail.com>
2014-09-10 21:31:20 +0400

    Preparation

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,80 @@
  * Controller of the simplexApp
  */
 angular.module('simplexApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+        .factory(
+                "Simplex",
+                function(trim) {
+                    // Define the constructor function.
+                    function Simplex() {
+                        var tableaux = [
+                            [0, 1, 3, 1, 0, 0, 6],
+                            [0, -1, 1, 0, 1, 0, 1],
+                            [0, 3, -1, 0, 0, 1, 6],
+                            [1, -1, -2, 0, 0, 0, 0],
+                        ];
+                        this.tableaux= tableaux;
+                    }
+
+
+                    // Define the "instance" methods using the prototype
+                    // and standard prototypal inheritance.
+                    Simplex.prototype = {
+                        solve: function() {
+
+                            return(this);
+
+                        },
+                        getTableaux: function(){
+                            return this.tableaux;
+                        }
+                    };
+
+
+                    // Define the "class" / "static" methods. These are
+                    // utility methods on the class itself; they do not
+                    /* have access to the "this" reference.
+                     Friend.fromFullName = function( fullName ) {
+                     
+                     var parts = trim( fullName || "" ).split( /\s+/gi );
+                     
+                     return(
+                     new Friend(
+                     parts[ 0 ],
+                     parts.splice( 0, 1 ) && parts.join( " " )
+                     )
+                     );
+                     
+                     
+                     };*/
+
+
+                    // Return constructor - this is what defines the actual
+                    // injectable in the DI framework.
+                    return(Simplex);
+
+                }
+        )
+        .value("trim", jQuery.trim)
+.filter('range', function() {
+  return function(input, total) {
+    total = parseInt(total);
+    for (var i=0; i<total; i++)
+      input.push(i);
+    return input;
+  };
+})
+
+
+
+        .controller('MainCtrl', function($scope, Simplex) {
+            $scope.awesomeThings = [
+                'HTML5 Boilerplate',
+                'AngularJS',
+                'Karma'
+            ];
+            var a = [
+            ];
+            var c = [1, 2];
+            var simplex = new Simplex();
+            $scope.tab = simplex.getTableaux();
+        });
